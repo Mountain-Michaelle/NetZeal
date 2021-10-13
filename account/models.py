fd2261ca60b7aac4b83ade040120a0612456599a
Mountain_Michelle <michaelchinemelu24@gmail.com>
2021-10-13 13:57:57 +0100

    register view added

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+"""# Create your models here.
+class RegForm(models.Model):
+
+    username = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=20)
+    email = models.EmailField() """
+
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse 
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -10,6 +11,30 @@ def dashboard(request):
     return render(request,
                     'account/dashboard.html',
                     {'section': 'dashboard'})
+
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            #set the chosen password 
+            new_user.set_password(
+                        user_form.cleaned_data['password'])
+            # save the user object 
+            new_user.save() 
+            return render(request, 
+                        'accoutn/register_done.html',
+                        {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                'account/register.html',
+                {'user_form':user_form})
+
+
 
 
 
@@ -39,7 +64,23 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form':form})
 
-
-
-
-
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(reqest.POST)
+        if user_form.is_valid():
+            # Create User object but don't save it.
+            new_user = user_form.save(commit=False)
+            #Set the chosen password 
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Now save the user object
+            new_user.save()
+            return render(request, 'account/register_done.html',
+            {'new-user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                    'account/register.html', {'user_form': user_form})
+                    
+            
