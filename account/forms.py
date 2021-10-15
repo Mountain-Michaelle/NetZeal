1c5c920945ae4c3069be5d73528f09346ad3e758
Mountain_Michelle <michaelchinemelu24@gmail.com>
2021-10-15 14:30:44 +0100

    corrected password confirmation error

--- account/forms.py
+++ account/forms.py
@@ -7,17 +7,16 @@ class LoginForm(forms.Form):
 
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',
-                            widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password',
-                            widget=forms.PasswordInput),
+class UsersRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='Confirm_password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email') 
-    def clean_password2(self):
+        fields = ('username', 'first_name','last_name', 'email', 'password') 
+        
+    def clean_confirm_password(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        if cd['password'] != cd['confirm_password']:
             raise forms.ValidationError('Passord doesn\'t match.')
-        return cd['password2']
+        return cd['confirm_password']
--- account/urls.py
+++ account/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
             name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
                 name='password_reset_confirm'),
-    path('reset/done', auth_views.PasswordResetCompleteView.as_view(),
-            name='password_reset_complete' ),
+    path('reset/done', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+    path('register/', views.register, name='register'),
+
 ]
